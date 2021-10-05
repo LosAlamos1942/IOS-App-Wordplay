9e7697554281cd8205bc6540e654f17cf7dba0c7
Nerto Kageh <kagehnerto@yahoo.com>
2021-10-05 10:21:39 -0500

    Initial Commit

---
+++ IOS App Wordplay/IOS_App_WordplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_App_WordplayApp.swift
+//  IOS App Wordplay
+//
+//  Created by Student on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS_App_WordplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
