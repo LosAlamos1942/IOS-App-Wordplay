3f0a576b4958acfb1d9e485a10f6941bbe80d64b
Nerto Kageh <kagehnerto@yahoo.com>
2021-10-12 10:48:20 -0500

    Kageh Nerto

---
+++ IOS App Wordplay/SwiftUIView1 .swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView1 .swift
+//  IOS App Wordplay
+//
+//  Created by Student on 10/6/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView1_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView1__Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView1_()
+    }
+}
